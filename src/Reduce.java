791dc47ddfe812608ed65ee154f1c167a0ebf541
paulxyan <168145928+paulxyan@users.noreply.github.com>
2024-09-13 15:39:22 -0400

    New Main Task 3 addition
    Multiples java exercise
    Multiples
    Added Reduce.java

---
+++ src/Reduce.java
@@ -0,0 +1,26 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int n = 100;
+
+        int steps = 0;
+
+        while (n > 0) {
+
+            if (n % 2 == 0){
+
+                n = n / 2;
+            }
+
+            else {
+
+                n = n - 1;
+            }
+
+            steps ++;
+
+        }
+
+        System.out.println("Amount of steps to reach 0: " + steps);
+    }
+}
